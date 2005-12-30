00cf2ccaf9d3b7836e9713df57657eb039cdfdf7
Lester Caine <lester@lsces.co.uk>
2005-12-30 17:46:47 +0000

    Add PHPGEDVIEW prefix selection for tables

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -6,4 +6,15 @@ $gBitSystem->registerPackage( 'phpgedview',  dirname( __FILE__ ).'/' );
 if( $gBitSystem->isPackageActive( PHPGEDVIEW_PKG_NAME ) ) {
 	$gBitSystem->registerAppMenu( PHPGEDVIEW_PKG_DIR, 'Genealogy', PHPGEDVIEW_PKG_URL.'index.php', 'bitpackage:phpgedview/menu_phpgedview.tpl', PHPGEDVIEW_PKG_NAME );
 }
+
+if( !defined( 'PHPGEDVIEW_DB_PREFIX' ) ) {
+	$lastQuote = strrpos( BIT_DB_PREFIX, '`' );
+	if( $lastQuote != FALSE ) {
+		$lastQuote++;
+	}
+	$prefix = substr( BIT_DB_PREFIX,  $lastQuote );
+	define( 'PHPGEDVIEW_DB_PREFIX', $prefix.'pgv_' );
+}
+
+$INDEX_DIRECTORY = 'c:\\Data\\';
 ?>
