7afa57cbd5fa8e8191180493b82964093e3bdec1
Lester Caine <lester@lsces.co.uk>
2006-10-28 21:46:21 +0000

    Enable bitweaver setup for alive in year viewer ( Need to ensure surnames have correct format )

--- aliveinyear.php
+++ aliveinyear.php
@@ -19,9 +19,20 @@
  *
  * @package PhpGedView
  * @subpackage Lists
- * @version $Id: aliveinyear.php,v 1.2 2006/10/01 22:44:01 lsces Exp $
+ * @version $Id: aliveinyear.php,v 1.3 2006/10/28 21:46:21 lsces Exp $
  */
 
+/**
+ * load the main configuration and context
+ */
+require_once( '../bit_setup_inc.php' );
+
+// Is package installed and enabled
+$gBitSystem->verifyPackage( 'phpgedview' );
+include_once( PHPGEDVIEW_PKG_PATH.'BitGEDCOM.php' );
+$gGedcom = new BitGEDCOM();
+
+// leave manual config until we can move it to bitweaver table 
 require("config.php");
 require_once("includes/functions_print_lists.php");
 
