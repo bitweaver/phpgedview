3dd5a4b93b4015d2b8bb4b994e4c2c323b72a387
wjames5 <will@tekimaki.com>
2009-10-01 13:45:45 +0000

    fix LGPL license ref from non-existant license.txt file to lgpl url

--- BitGEDCOM.php
+++ BitGEDCOM.php
@@ -7,7 +7,7 @@
  *
  * Copyright (c) 2004 bitweaver.org
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
  */
 
 /**
--- admin/admin_phpgedview_inc.php
+++ admin/admin_phpgedview_inc.php
@@ -1,7 +1,7 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_phpgedview/admin/admin_phpgedview_inc.php,v 1.3 2006/04/14 20:25:52 squareing Exp $
+// $Header: /cvsroot/bitweaver/_bit_phpgedview/admin/admin_phpgedview_inc.php,v 1.4 2009/10/01 13:45:45 wjames5 Exp $
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 $calendar = array( "gregorian", "julian", "french", "jewish", 
 	"jewish_and_gregorian", "hebrew", "hebrew_and_gregorian" );
